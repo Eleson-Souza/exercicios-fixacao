1f6728686edf4760c79b2a1d1ff9e1a4966eb552
Eleson Souza <elesonsouza17@hotmail.com>
2020-05-05 23:35:15 -0300

    Adicionando primeiros arquivos, referentes ao exercício de conversão de valores binários para decimais

---
+++ script.js
@@ -0,0 +1,27 @@
+const resultado = document.getElementById('resultado');
+const input = document.getElementById('binary');
+
+// Função responsável por capturar e converter o valor binário em decimal.
+function convertBinaryToDecimal() {
+    const valorStr = document.getElementById('binary').value;
+    var arrayValor = valorStr.split('');
+    // Percorrendo e atualizando array para numérico.
+    arrayValor = arrayValor.map(value => { return parseInt(value) });
+    
+    var acumDec = 0;
+    var exp = valorStr.length - 1; // expoente
+    for(var i = 0; i < valorStr.length; i++) {
+        acumDec += (arrayValor[i] * Math.pow(2, exp));
+        exp--;
+    }
+    // Exibindo no elemento do HTML o resultado da conversão.
+    resultado.innerText = acumDec;
+}
+
+// Função valida se o usuário está informando apenas 0 ou 1 a partir dos seus respectivos códigos na tabela ASCii.
+function validarTecla(event) {
+    if(event.keyCode != 48 && event.keyCode != 49) {
+        event.preventDefault();
+        alert('Caractere inválido, informe apenas 0 ou 1!');
+    }
+}
